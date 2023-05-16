d99dfecc23fb69c3c87db3e7452da29ef3cc352a
Mridul Bagla <53037689+MRIDUL-MB@users.noreply.github.com>
2023-05-16 22:43:51 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route('/api/email', methods=['POST'])
+def send_email():
+    data = request.get_json()
+    
+    # Extract data from the request
+    recipient = data.get('recipient')
+    subject = data.get('subject')
+    body = data.get('body')
 
-@app.route('/about')
-def about():
-    return 'About'
+    print(body)
+    
+    # Code to send the email using your preferred email service provider
+    
+    # Return a response
+    response = {'message': 'Email sent successfully'}
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run()
