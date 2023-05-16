e99ac9e8e14eb631c4e793a75370da9454d5580a
mridul-mb <mriravbagla@gmail.com>
2023-05-16 22:59:09 +0530

    initailized

--- api/index.py
+++ api/index.py
@@ -2,22 +2,36 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+html_content = ""
+
+
+@app.route('/api/get', methods=['GET'])
+def get_date():
+    global html_content
+    response = {'message': 'Date sent successfully', 'data': html_content}
+    return jsonify(response), 200
+
+
 @app.route('/api/email', methods=['POST'])
 def send_email():
     data = request.get_json()
-    
+
     # Extract data from the request
     recipient = data.get('recipient')
     subject = data.get('subject')
     body = data.get('body')
+    html_content = data.get(
+        'html_content'
+    )
 
-    print(body)
-    
+    print(html_content)
+
     # Code to send the email using your preferred email service provider
-    
+
     # Return a response
-    response = {'message': 'Email sent successfully'}
+    response = {'message': 'Email sent successfully', 'data': html_content}
     return jsonify(response), 200
+
 
 if __name__ == '__main__':
     app.run()
